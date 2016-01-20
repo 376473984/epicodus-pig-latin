001df115f885cd16ecbbd28bad7f07d78bf39a35
Joey Arnstein and Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-20 10:34:54 -0800

    more comments in scripts.js on if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,19 @@
 
 var translator = function(userText) {
   var newString;
-// for words starting with one consonant
+// for words that do not start with a vowel
   if (!userText.match(/^[aeiou]/i)) {
+    // if the second letter is also not a vowel
     if (!userText[1].match(/[aeiou]/i)) {
       var beginningConsonants = userText.slice(0, 2);
       newString = userText.slice(2) + beginningConsonants;
     } else {
+      // if the second letter is a vowel
     var firstLetter = userText.slice(0, 1);
     newString = userText.slice(1) + firstLetter;
     }
   } else {
+    // if the first letter is a vowel
     newString = userText;
   }
 
