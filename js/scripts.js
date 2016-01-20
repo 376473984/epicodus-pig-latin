42e0cb6a6421fc7a5ffcff4537e4631b2c2309a1
Joey Arnstein and Michelle Brecunier <joeyarnstein@gmail.com>
2016-01-20 10:56:04 -0800

    add for loop to handle words that start with multiple consonants

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,11 @@ var translator = function(userText) {
   var newString;
 // for words that do not start with a vowel
   if (!userText.match(/^[aeiou]/i)) {
-    // if the second letter is also not a vowel
-    if (!userText[1].match(/[aeiou]/i)) {
-      var beginningConsonants = userText.slice(0, 2);
-      newString = userText.slice(2) + beginningConsonants;
-    } else {
-      // if the second letter is a vowel
-    var firstLetter = userText.slice(0, 1);
-    newString = userText.slice(1) + firstLetter;
+    for (var index = 0; !userText[index].match(/[aeiou]/i); index ++) {
+      // !userText[index].match(/[aeiou]/i)
+      var beginningConsonants = userText.slice(0, index+1);
     }
+    newString = userText.slice(index) + beginningConsonants;
   } else {
     // if the first letter is a vowel
     newString = userText;
